2f790ee3caf7280afbeef72a54ff47add2dd60dc
Micah Sherr <msherr@cs.georgetown.edu>
2013-10-31 22:57:03 -0400

    first version

---
+++ src/gen-syllabus.py
@@ -0,0 +1,73 @@
+#!/usr/bin/python
+
+# A syllabus creator by Micah Sherr
+# Georgetown University
+# msherr@cs.georgetown.edu
+
+
+import dateutil.parser
+import argparse
+import datetime
+import yaml
+from jinja2 import Template
+
+
+def parse_command_line():
+    parser = argparse.ArgumentParser(description='Syllabus generator')
+    parser.add_argument('--holidays', help='list of holidays, separated by ":"')
+    parser.add_argument('--start', help='start date', required=True)
+    parser.add_argument('--end', help='end date', required=True)
+    parser.add_argument('--days', help='days of week (0-6, where Monday is 0 and Sunday is 0)', required=True)
+    parser.add_argument('--schedule', help='schedule file', required=True)
+    parser.add_argument('--template', help='django template file', required=True)
+    args = parser.parse_args()
+    return args
+
+
+
+def main():
+    args = parse_command_line()
+    start = dateutil.parser.parse(args.start).date()
+    end = dateutil.parser.parse(args.end).date()
+    days = list(args.days)
+
+    default_description = { 'description' : 'TBD' }
+    
+    with file(args.schedule, 'rt') as f:
+        schedule = yaml.load(f)
+    with open(args.template,'rt') as f:
+        t = Template(f.read())
+
+    if args.holidays is not None:
+        holidays = map( lambda x : dateutil.parser.parse(x).date(), args.holidays.split(':'))
+    else:
+        holidays = []
+
+    day_count = (end - start).days + 1
+    class_num = 0
+    for single_date in (start + datetime.timedelta(n) for n in range(day_count)):
+        day_of_week = str(single_date.weekday())
+        if day_of_week in days:
+            if single_date in holidays:
+                pass
+                #                print "-|" + str(single_date) + "|No class"
+            else:
+                if class_num < len(schedule):
+                    class_info = schedule[class_num]
+                else:
+                    class_info = default_description
+                class_num = class_num + 1
+                if 'readings' not in class_info:
+                    readings = "None"
+                else:
+                    # TODO
+                    pass
+                    #                print "|".join((str(class_num), str(single_date), class_info['description'],readings))
+                class_info['lec_num'] = class_num
+                class_info['lec_date'] = single_date
+                print t.render(class_info)
+
+
+
+if __name__ == "__main__":
+    main()
