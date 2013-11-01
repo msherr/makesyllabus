6cfffc3307c1db7b9c3380936a934e3be75b7c57
Micah Sherr <msherr@cs.georgetown.edu>
2013-10-31 23:07:08 -0400

    now with headers and footers!

--- src/make-syllabus.py
+++ src/make-syllabus.py
@@ -20,6 +20,8 @@ def parse_command_line():
     parser.add_argument('--days', help='days of week (0-6, where Monday is 0 and Sunday is 0)', required=True)
     parser.add_argument('--schedule', help='schedule file', required=True)
     parser.add_argument('--template', help='django template file', required=True)
+    parser.add_argument('--header', help='header file to insert before template')
+    parser.add_argument('--footer', help='footer file to insert before template')
     args = parser.parse_args()
     return args
 
@@ -43,30 +45,32 @@ def main():
     else:
         holidays = []
 
+    if args.header is not None:
+        with open(args.header,'rt') as h:
+            print h.read()
+    
     day_count = (end - start).days + 1
     class_num = 0
     for single_date in (start + datetime.timedelta(n) for n in range(day_count)):
         day_of_week = str(single_date.weekday())
         if day_of_week in days:
             if single_date in holidays:
-                pass
-                #                print "-|" + str(single_date) + "|No class"
+                class_info['lec_num'] = "-"
+                class_info['lec_date'] = single_date
+                class_info['description'] = 'No class'
             else:
                 if class_num < len(schedule):
                     class_info = schedule[class_num]
                 else:
                     class_info = default_description
                 class_num = class_num + 1
-                if 'readings' not in class_info:
-                    readings = "None"
-                else:
-                    # TODO
-                    pass
-                    #                print "|".join((str(class_num), str(single_date), class_info['description'],readings))
                 class_info['lec_num'] = class_num
                 class_info['lec_date'] = single_date
-                print t.render(class_info)
+            print t.render(class_info)
 
+    if args.footer is not None:
+        with open(args.footer,'rt') as f:
+            print f.read()
 
 
 if __name__ == "__main__":
